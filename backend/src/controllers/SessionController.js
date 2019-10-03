115bbe8f5d125a927059d1750113a7f631465ff8
Jonatas Pedroso <jonatasarlindo@hotmail.com>
2019-10-03 02:03:33 -0300

    Finalizado o FrontEnd

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 module.exports = {
     async store(req, res) {
         const {email} = req.body;
-        let user = await user.findOne({email});
+        let user = await User.findOne({email});
         if (!user){
             user = await User.create({ email });
         }        
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const server = express();
 
-const uri = 'mongodb+srv://omnistack:omnistack@cluster0-u49dg.mongodb.net/omnistack9?w=majority';
+const uri = 'mongodb+srv://omnistack:omnistack@cluster0-u49dg.mongodb.net/omnistack9?retryWrites=true&w=majority';
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).catch((err) => console.error(err));
 
+server.use(cors());
 server.use(express.json());
+server.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 server.use(routes);
 
 server.listen(3333);
